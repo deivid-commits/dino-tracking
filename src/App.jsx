6950f4f49cc4bb7afc45b5fdfa5853f4766a9bb3
deividsten <deivisten@gmail.com>
2025-10-09 15:02:18 -0700

    FASE 3: Inicializar proyecto de React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>¡Hola, Mundo!</h1>
+      <p>Esta es nuestra nueva aplicación de React con Vite y Supabase.</p>
+    </div>
+  );
+}
+
+export default App;
